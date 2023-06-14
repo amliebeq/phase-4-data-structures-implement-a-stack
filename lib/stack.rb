30c1d342bfe0e2745917a9b798e88f870b1a0f67
alex lieberman <alex.lieberman182@gmail.com>
2023-06-14 09:56:35 -0700

    commit tree 27296fe7235d68aaca5867b31bab8b22beb6ba37

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,17 @@
-# your code here
+class Stack
+    def initialize(stack = [])
+        @stack = stack
+    end
+
+    def push(value)
+        @stack << value
+    end
+
+    def pop 
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end
